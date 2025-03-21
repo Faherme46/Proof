9d1de81d45d6fe1f66f26e7c39bbce68b2b54503
Faherme46 <faherme46@gmail.com>
2025-03-21 12:15:49 -0500

    1

--- proof.py
+++ proof.py
@@ -1,5 +1,5 @@
 import hid
-
+import os
 
 vid = 4292  # Reemplazar con tu vendor_id
 pid = 6169  # Reemplazar con tu product_id
@@ -7,6 +7,8 @@ def connect():
     try:
         # Abrir conexión con el dispositivo
         device = hid.device()
+        print(device)
+        os.system('pause')
         device.open(vid, pid)
         device.set_nonblocking(1)  # Configurar modo no bloqueante
         return device
@@ -14,3 +16,8 @@ def connect():
     except OSError as e:
         print(f"Error al conectar con el dispositivo HID: {e}")
         return False
+    
+
+# Lista todos los dispositivos HID conectados
+for dispositivo in hid.enumerate():
+    print(f"VID: {dispositivo['vendor_id']} | PID: {dispositivo['product_id']} | Producto: {dispositivo['product_string']}")
